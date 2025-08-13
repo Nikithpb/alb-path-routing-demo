f2d4ccf4b9ba692ba4be7f19a1338cbbb3f7469e
Nikithpb <nikithparlakoti66@gmail.com>
2025-08-13 04:53:49 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hello from Flask!</h1>"
+
+@app.route('/5000')
+def flask5000():
+    return "<h1>Hello from Flask at /5000!</h1>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
